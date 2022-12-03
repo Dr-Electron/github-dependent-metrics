924da04d97af591bbdbeb2af319f70ad1b99b7bd
Dr-Electron <dr-electr0n@protonmail.com>
2022-12-03 23:46:22 +0100

    Fix and update rust version detector

--- version.py
+++ version.py
@@ -2,8 +2,6 @@ import os
 import re
 import tomllib
 
-regex = r'(?:\?|\&)(?:\=|\&?)((rev=)+?(?P<revison>[\w|\d]+)|(branch=)+?(?P<branch>[\w|\d]+))*(?P<commit>#(\w|\d)+){0,}'
-
 def has_same_path(path1, path2):
     return os.path.split(path1)[0] == os.path.split(path2)[0]
 
@@ -25,27 +23,49 @@ def get_matching_files(cache, full_name: str, pattern):
     return files
 
 def get_rust_lib_version(cache, full_name: str, package_name):
-    files = get_matching_files(cache, full_name, ['Cargo.toml', 'Cargo.lock'])
+    files = get_matching_files(cache, full_name, 'Cargo.toml')
     versions = []
+    root_lock_dependent = None
     index = 0
     while index < len(files):
         current_index = index
         index += 1
 
+        file = files[current_index]
+        split_file = file.path.split('/')
+
+        split_file[-1] = 'Cargo.lock'
+        lock_file_path = '/'.join(split_file) 
+        lock_file = cache.get_content(full_name, lock_file_path)
+
         lock_dependent = {}
-        if files[current_index].path.endswith(".lock"):
+        if lock_file:
             lock_file_dict = tomllib.loads(
-                files[current_index].decoded_content.decode()
+                lock_file.decoded_content.decode()
             )
-            lock_dependent = next(
+            lock_file_dependent = next(
                 (package for package in lock_file_dict.get('package') if package.get("name") == package_name), 
                 {}
             )
+
+            if bool(lock_file_dependent):
+                lock_dependent['version'] = lock_file_dependent.get('version')
+                if lock_file_dependent.get('source').startswith('git'):
+                    match_dict = re.search(
+                        r'(?:\?|\&)(?:\=|\&?)((rev=)+?(?P<rev>[\w|\d]+)|(branch=)+?(?P<branch>[\w|\d]+))#*(?P<commit>[\w|\d]+){0,}', 
+                        lock_file_dependent.get('source')).groupdict() if (lock_dependent) else {}
+                    lock_dependent['rev'] = match_dict['rev'] if match_dict.get('rev') else match_dict.get('commit')
+                    lock_dependent['branch'] = match_dict['branch']
+
+        if len(split_file) == 1:
+            root_lock_dependent = lock_dependent
 
         manifest_dependent = {}
         manifest_file = None
         if files[current_index].path.endswith(".toml"):
             manifest_file = files[current_index]
+            if not lock_file:
+                lock_dependent = root_lock_dependent
 
         if current_index+1 < len(files) and has_same_path(files[current_index].path, files[current_index+1].path):
             index += 1
@@ -59,17 +79,13 @@ def get_rust_lib_version(cache, full_name: str, package_name):
         if not manifest_dependent and not lock_dependent:
             continue
 
-        match_dict = {}
-        if lock_dependent.get('source').startswith('git'):
-            match_dict = re.search(regex, lock_dependent.get('source')).groupdict() if (lock_dependent) else {}
-
         versions.append({
             'version': manifest_dependent.get('version'),
             'rev': manifest_dependent.get('rev'),
             'branch': manifest_dependent.get('branch'),
             'locked_version': lock_dependent.get('version'),
-            'locked_rev': match_dict.get('revison'),
-            'locked_branch': match_dict.get('branch'),
+            'locked_rev': lock_dependent.get('rev'),
+            'locked_branch': lock_dependent.get('branch'),
         })
 
     return versions
